489468bb7199ee97826618c359ad1aa870c8fa31
maryna kotelnikova <neveroer@gmail.com>
2021-03-04 19:22:07 +0200

    Updates

---
+++ static/js/main.1c16a3ec.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpproject_v=this.webpackJsonpproject_v||[]).push([[0],[function(t,c,p){"use strict";p.r(c);p(1)},function(t,c,p){}],[[0,1]]]);
+//# sourceMappingURL=main.1c16a3ec.chunk.js.map
